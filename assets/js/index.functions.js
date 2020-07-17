94bfe4328252621330d7f5a40d8d42f926605cfc
Gabriel Souza <gabrielspereira2604@gmail.com>
2020-07-17 01:00:57 -0300

    Some adjustings

--- assets/js/index.functions.js
+++ assets/js/index.functions.js
@@ -37,12 +37,3 @@ $.ajax({
       $('.ajax-reponse-select-warning').html(response);        
   },
 });
-
-document.onmousedown=disableclick;
-function disableclick(event)
-{
-  if(event.button==2)
-   {
-     return false;    
-   }
-}
